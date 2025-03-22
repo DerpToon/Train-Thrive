52c5b1f128eae5cc0d26c3736b667a5d49f29f65
MahmoudSidawi <mahsid1234@gmail.com>
2025-03-22 12:28:23 +0200

    nnn
    m

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,43 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/workouts', function () {
+    return view('workout');
+})->name('workouts');
+
+Route::get('/calculator', function () {
+    return view('calculator');
+})->name('calculator');
+
+Route::get('/shop', function () {
+    return view('products');
+})->name('shop');
+
+Route::get('/about-us', function () {
+    return view('about-us');
+})->name('about');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/cart', function () {
+    return view('cart');
+})->name('cart');
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
+
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
 Route::get('/', function () {
     return view('welcome');
 });
