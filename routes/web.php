fea6286855f79d5934d96251b76055446536b76f
MahmoudSidawi <mahsid1234@gmail.com>
2025-03-22 12:03:55 +0200

    sss

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    return view('welcome');
+});
 
-Route::get('/workouts', function () {
-    return view('workout');
-})->name('workouts');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/calculator', function () {
-    return view('calculator');
-})->name('calculator');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/shop', function () {
-    return view('products');
-})->name('shop');
-
-Route::get('/about-us', function () {
-    return view('about-us');
-})->name('about');
-
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-Route::get('/cart', function () {
-    return view('cart');
-})->name('cart');
-
-Route::get('/products', function () {
-    return view('products');
-})->name('products');
-
-
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+require __DIR__.'/auth.php';
