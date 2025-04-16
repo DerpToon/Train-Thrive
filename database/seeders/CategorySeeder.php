c164dbe93e3304f4e96ea0c17ef2726fe736f87f
sabah-tech <AjlouniSY@students.rhu.edu.lb>
2025-04-16 22:10:19 +0300

    .

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -13,9 +14,18 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         //
-        Category::create([
-            'name' => 'Protein Powders',
-            'image' => 'imgs/protein.png',
-        ]);
+        $categories = [
+            ['name' => 'Protein Powders', 'image' => 'Images/Products/protein.jpg'],
+            ['name' => 'Flakes', 'image' => 'Images/Products/flakes.jpg'],
+            ['name' => 'Mass Gainers', 'image' => 'imgs/Products/mass.jpg'],
+            ['name' => 'Meal Replacements', 'image' => '/imgs/Products/mealreplacement.jpg'],
+            ['name' => 'Protein Bars', 'image' => '/imgs/Products/proteinbar.jpg'],
+            ['name' => 'Protein Snacks', 'image' => '/imgs/Products/snacks.jpg'],
+            ['name' => 'Energy Flavors', 'image' => '/public/imgs/Products/energy.jpg'],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Category;
 
 class ProductSeeder extends Seeder
 {
@@ -13,5 +15,22 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         //
+        $categories = Category::all()->keyBy('name');
+
+        $products = [
+            ['name' => 'Whey Isolate', 'price' => 29.99, 'image' => 'imgs/Products Images/1.png', 'category' => 'Protein Powders'],
+            ['name' => 'Chocolate Gainer', 'price' => 34.99, 'image' => 'Images/Products/gainer.jpg', 'category' => 'Mass Gainers'],
+            ['name' => 'Banana Energy', 'price' => 9.99, 'image' => 'Images/Products/banana.jpg', 'category' => 'Energy Flavors'],
+            ['name' => 'Oats Flakes', 'price' => 5.99, 'image' => 'Images/Products/oats.jpg', 'category' => 'Flakes'],
+        ];
+
+        foreach ($products as $product) {
+            Product::create([
+                'name' => $product['name'],
+                'price' => $product['price'],
+                'image' => $product['image'],
+                'category_id' => $categories[$product['category']]->id,
+            ]);
+        }
     }
 }
